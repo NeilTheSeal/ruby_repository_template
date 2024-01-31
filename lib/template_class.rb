98ee8f3142c8da3f0af2aae45fae333fc215a694
Neil Hendren <neil.hendren@gmail.com>
2024-01-31 11:27:22 -0700

    Add class and specs

---
+++ lib/template_class.rb
@@ -0,0 +1,3 @@
+# Documentation for TemplateClass
+class TemplateClass
+end
---
+++ spec/template_class_spec.rb
@@ -0,0 +1,9 @@
+require "rspec"
+require "./lib/template_class"
+
+RSpec.describe TemplateClass do
+  it "exists" do
+    template_class = TemplateClass.new
+    expect(template_class).to be_an_instance_of(TemplateClass)
+  end
+end
